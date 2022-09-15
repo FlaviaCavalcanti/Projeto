bfa33208fe28b015b25a094054972e26831899da
mayara <flavia.mayara2503@gmail.com>
2022-09-15 13:40:28 -0300

    Concluindo o blog pessoal

---
+++ src/app/model/UserLogin.ts
@@ -0,0 +1,14 @@
+export class UserLogin{
+    public id: number
+    public nome: string
+    public usuario: string
+    public senha: string
+    public token: string
+    public foto: string 
+    public tipo: string 
+}
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privac
